097933b768ab8c6127b857eb151423641c53e399
Kevillin Santos <kevillin_santos@hotmail.com>
2022-08-03 16:26:09 -0300

    Requisito 3

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Header extends Component {
+  state = {
+    userInput: '',
+    loading: false,
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({
+        userInput: user.name,
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { userInput, loading } = this.state;
     return (
-      <header data-testid="header-component">Header</header>
+      <>
+        <header data-testid="header-component">
+          <div data-testid="header-user-name">{userInput}</div>
+          <Link to="/Search">Search</Link>
+        </header>
+
+        {loading ? <Loading /> : null}
+      </>
     );
   }
 }
