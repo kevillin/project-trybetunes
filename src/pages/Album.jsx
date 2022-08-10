508e811e263ab9328d0306fe27137d3fc0e032ad
Kevillin Santos <kevillin_santos@hotmail.com>
2022-08-10 19:19:52 -0300

    Requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
 
 export default class Album extends Component {
   state = {
-    // albunsTodos: [],
+    albunsTodos: [],
     primeiroAlbum: {},
     // carregando: false,
   };
@@ -17,9 +18,8 @@ export default class Album extends Component {
   puxaMusica = async () => {
     const { match: { params: { id } } } = this.props;
     const response = await getMusics(id);
-    console.log(await response);
     this.setState({
-      // albunsTodos: response,
+      albunsTodos: response,
       primeiroAlbum: response[0],
     });
   };
@@ -27,6 +27,7 @@ export default class Album extends Component {
   render() {
     const {
       primeiroAlbum,
+      albunsTodos,
     } = this.state;
     return (
       <>
@@ -35,6 +36,14 @@ export default class Album extends Component {
           <h1 data-testid="album-name">{ primeiroAlbum.collectionName }</h1>
           <p data-testid="artist-name">{ primeiroAlbum.artistName }</p>
         </div>
+        {albunsTodos.map((music, index) => index !== 0 && (
+          <span key={ index }>
+            <MusicCard
+              trackName={ music.trackName }
+              previewUrl={ music.previewUrl }
+            />
+          </span>
+        ))}
       </>
     );
   }
