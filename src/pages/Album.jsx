3b92cabe0ed48f956541fedf30462c0118ca4a19
Kevillin Santos <kevillin_santos@hotmail.com>
2022-08-14 19:18:51 -0300

    Requisito 8 - com ajuda do Isaque Almeida - Turma 23B

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from './MusicCard';
+import Loading from './Loading';
 
 export default class Album extends Component {
   state = {
     albunsTodos: [],
     primeiroAlbum: {},
-    // carregando: false,
+    loading: false,
   };
 
   componentDidMount() {
@@ -16,11 +17,13 @@ export default class Album extends Component {
   }
 
   puxaMusica = async () => {
+    this.setState({ loading: true });
     const { match: { params: { id } } } = this.props;
     const response = await getMusics(id);
     this.setState({
       albunsTodos: response,
       primeiroAlbum: response[0],
+      loading: false,
     });
   };
 
@@ -28,10 +31,12 @@ export default class Album extends Component {
     const {
       primeiroAlbum,
       albunsTodos,
+      loading,
     } = this.state;
     return (
       <>
         <Header />
+        {loading ? <Loading /> : null}
         <div data-testid="page-album">
           <h1 data-testid="album-name">{ primeiroAlbum.collectionName }</h1>
           <p data-testid="artist-name">{ primeiroAlbum.artistName }</p>
@@ -39,8 +44,7 @@ export default class Album extends Component {
         {albunsTodos.map((music, index) => index !== 0 && (
           <span key={ index }>
             <MusicCard
-              trackName={ music.trackName }
-              previewUrl={ music.previewUrl }
+              music={ music }
             />
           </span>
         ))}
--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,23 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
+  state = {
+    loading: false,
+    trechosFav: [],
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const favorite = await getFavoriteSongs();
+    this.setState({
+      loading: false,
+      trechosFav: favorite,
+    });
+  }
+
+  pegaFavorito = async (music, event) => {
+    event.preventDefault();
+    if (event.target.checked) {
+      this.setState({ loading: true });
+      await addSong(music);
+      const resgataFavorita = await getFavoriteSongs();
+      this.setState({
+        loading: false,
+        trechosFav: resgataFavorita,
+      });
+    }
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { music } = this.props;
+    const { trackName, previewUrl, trackId } = music;
+    const { loading, trechosFav } = this.state;
     return (
       <div>
+        {loading ? <Loading /> : null}
         <h3>{trackName}</h3>
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           {`O seu navegador não suporta o elemento ${trackName}`}
           <code>audio</code>
         </audio>
+        <label htmlFor="favorite-song">
+          <h5>Favoritar</h5>
+          <input
+            type="checkbox"
+            name="favorite-song"
+            data-testid={ `checkbox-music-${trackId}` }
+            checked={ trechosFav.some((fav) => (
+              fav.trackId === music.trackId
+            )) }
+            onChange={ (event) => this.pegaFavorito(music, event) }
+          />
+        </label>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  trackName: PropTypes.string.isRequired,
-  previewUrl: PropTypes.string.isRequired,
+  music: PropTypes.shape({
+    trackName: PropTypes.string,
+    previewUrl: PropTypes.string,
+    trackId: PropTypes.number,
+  }).isRequired,
 };
